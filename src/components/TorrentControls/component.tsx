18a07fbbfbfab6e26b9396c7f0325ee2deccbad7
Umar Farooq Khawaja <umar.farooq.khawaja@gmail.com>
2025-08-10 22:06:50 +0100

    feat: link action icons to corresponding actions

--- src/components/TorrentControls/component.tsx
+++ src/components/TorrentControls/component.tsx
@@ -1,23 +1,34 @@
 import { Group } from '@mantine/core';
-import { IconArrowDown, IconArrowUp, IconLink, IconPlayerPlay, IconPlayerStop } from '@tabler/icons-react';
+import { IconArrowBarDown, IconArrowBarUp, IconLink, IconPlayerPlay, IconPlayerStop } from '@tabler/icons-react';
+import { useActions, useTorrent } from '../../providers';
 import { ActionButton } from '../ActionButton';
 
 export function TorrentControls() {
+  const {
+    startTorrent,
+    stopTorrent,
+    moveTorrentUp,
+    moveTorrentDown,
+    copyTorrentLink
+  } = useActions();
+
+  const { torrent } = useTorrent();
+
   return (
     <Group gap="xs">
-      <ActionButton>
+      <ActionButton onClick={(): Promise<void> => startTorrent(torrent.id)}>
         <IconPlayerPlay/>
       </ActionButton>
-      <ActionButton>
+      <ActionButton onClick={(): Promise<void> => stopTorrent(torrent.id)}>
         <IconPlayerStop/>
       </ActionButton>
-      <ActionButton>
-        <IconArrowUp/>
+      <ActionButton onClick={(): Promise<void> => moveTorrentUp(torrent.id)}>
+        <IconArrowBarUp/>
       </ActionButton>
-      <ActionButton>
-        <IconArrowDown/>
+      <ActionButton onClick={(): Promise<void> => moveTorrentDown(torrent.id)}>
+        <IconArrowBarDown/>
       </ActionButton>
-      <ActionButton>
+      <ActionButton onClick={(): Promise<void> => copyTorrentLink(torrent.id)}>
         <IconLink/>
       </ActionButton>
     </Group>
