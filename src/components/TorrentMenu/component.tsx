44f54f86291c12070d85b6edf2144cab57e457bb
Umar Farooq Khawaja <umar.farooq.khawaja@gmail.com>
2025-08-10 21:02:36 +0100

    feat: add more menu items to TorrentMenu component

--- src/components/TorrentMenu/component.tsx
+++ src/components/TorrentMenu/component.tsx
@@ -1,5 +1,14 @@
-import { Menu } from '@mantine/core';
-import { IconDotsVertical } from '@tabler/icons-react';
+import { Menu, ThemeIcon } from '@mantine/core';
+import {
+  IconChecks,
+  IconDots,
+  IconFolder,
+  IconLabel,
+  IconPencil,
+  IconSpeakerphone,
+  IconTrash,
+  IconTrashX
+} from '@tabler/icons-react';
 import { ActionButton } from '../ActionButton';
 
 export function TorrentMenu() {
@@ -7,12 +16,75 @@ export function TorrentMenu() {
     <Menu>
       <Menu.Target>
         <ActionButton>
-          <IconDotsVertical/>
+          <IconDots/>
         </ActionButton>
       </Menu.Target>
       <Menu.Dropdown>
-        <Menu.Item>Remove from list</Menu.Item>
-        <Menu.Item>Delete downloaded files and remove from list</Menu.Item>
+        <Menu.Item
+          leftSection={(
+            <ThemeIcon variant="transparent" c="green">
+              <IconChecks/>
+            </ThemeIcon>
+          )}
+        >
+          Verify local data
+        </Menu.Item>
+        <Menu.Item
+          leftSection={(
+            <ThemeIcon variant="transparent" c="gray">
+              <IconFolder/>
+            </ThemeIcon>
+          )}
+        >
+          Set location...
+        </Menu.Item>
+        <Menu.Item
+          leftSection={(
+            <ThemeIcon variant="transparent" c="gray">
+              <IconPencil/>
+            </ThemeIcon>
+          )}
+        >
+          Rename...
+        </Menu.Item>
+        <Menu.Item
+          leftSection={(
+            <ThemeIcon variant="transparent" c="gray">
+              <IconLabel/>
+            </ThemeIcon>
+          )}
+        >
+          Edit labels...
+        </Menu.Item>
+        <Menu.Divider/>
+        <Menu.Item
+          leftSection={(
+            <ThemeIcon variant="transparent" c="gray">
+              <IconTrash/>
+            </ThemeIcon>
+          )}
+        >
+          Remove from list
+        </Menu.Item>
+        <Menu.Item
+          leftSection={(
+            <ThemeIcon variant="transparent" c="red">
+              <IconTrashX/>
+            </ThemeIcon>
+          )}
+        >
+          Delete downloaded files and remove from list
+        </Menu.Item>
+        <Menu.Divider/>
+        <Menu.Item
+          leftSection={(
+            <ThemeIcon variant="transparent" c="gray">
+              <IconSpeakerphone/>
+            </ThemeIcon>
+          )}
+        >
+          Ask tracker for more peers
+        </Menu.Item>
       </Menu.Dropdown>
     </Menu>
   );
