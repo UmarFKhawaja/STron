8815adaee9f95f81c20e1b37a20bd776ae97c983
Umar F Khawaja <umar.farooq.khawaja@gmail.com>
2025-08-11 17:36:26 +0100

    feat: improve UI/UX of the TorrentCard component

--- src/components/TorrentsGrid/components/TorrentCards/components/TorrentCard/component.tsx
+++ src/components/TorrentsGrid/components/TorrentCards/components/TorrentCard/component.tsx
@@ -1,39 +1,40 @@
-import { Box, Group, Paper, Stack, Text } from '@mantine/core';
+import { Group, Paper, Stack, Text } from '@mantine/core';
 import { useTorrent } from '../../../../../../providers';
 import { TorrentControls } from '../../../../../TorrentControls';
 import { TorrentMenu } from '../../../../../TorrentMenu';
 import { ETALabel, InfoDisplay, ProgressDisplay, RatioLabel, StatusLabel } from './components';
-import classes from './styles.module.css';
 
 export function TorrentCard() {
   const { torrent } = useTorrent();
 
   return (
     <Paper withBorder p="lg">
-      <Box className={classes.inner}>
-        <Stack gap="sm">
-          <Text size="lg" fw={600}>
-            {torrent.name}
-          </Text>
-          <Group gap="sm">
-            <InfoDisplay mode="downloadedBytes"/>
-            <InfoDisplay mode="uploadedBytes"/>
-            <InfoDisplay mode="downloadRate"/>
-            <InfoDisplay mode="uploadRate"/>
-          </Group>
-          <Group gap="sm">
-            <ETALabel/>
-            <RatioLabel/>
-          </Group>
-          <Group gap="sm">
-            <StatusLabel/>
-          </Group>
-        </Stack>
-        <ProgressDisplay/>
-      </Box>
-      <Group gap="sm">
-        <TorrentControls/>
-        <TorrentMenu/>
+      <Stack gap="sm">
+        <Text size="lg" fw={600}>
+          {torrent.name}
+        </Text>
+        <Group gap="sm" justify="space-between">
+          <Stack gap="sm">
+            <Group gap="sm">
+              <ETALabel/>
+              <RatioLabel/>
+            </Group>
+            <Group gap="sm">
+              <InfoDisplay mode="downloadedBytes"/>
+              <InfoDisplay mode="uploadedBytes"/>
+              <InfoDisplay mode="downloadRate"/>
+              <InfoDisplay mode="uploadRate"/>
+            </Group>
+          </Stack>
+          <ProgressDisplay/>
+        </Group>
+      </Stack>
+      <Group gap="sm" justify="space-between">
+        <Group gap="sm">
+          <TorrentControls/>
+          <TorrentMenu/>
+        </Group>
+        <StatusLabel/>
       </Group>
     </Paper>
   );
--- src/components/TorrentsGrid/components/TorrentCards/components/TorrentCard/components/ProgressDisplay/component.tsx
+++ src/components/TorrentsGrid/components/TorrentCards/components/TorrentCard/components/ProgressDisplay/component.tsx
@@ -1,6 +1,5 @@
-import { Box, RingProgress, Text } from '@mantine/core';
+import { RingProgress, Text } from '@mantine/core';
 import { useTorrent } from '../../../../../../../../providers';
-import classes from './styles.module.css';
 
 export function ProgressDisplay() {
   const { torrent } = useTorrent();
@@ -8,23 +7,21 @@ export function ProgressDisplay() {
   const doneProportion: number = torrent.percentComplete * 100;
 
   return (
-    <Box className={classes.ring}>
-      <RingProgress
-        roundCaps
-        thickness={6}
-        size={150}
-        sections={[{ value: doneProportion, color: doneProportion < 100 ? 'blue' : 'green' }]}
-        label={
-          <div>
-            <Text ta="center" fz="lg" className={classes.label}>
-              {doneProportion.toFixed(0)}%
-            </Text>
-            <Text ta="center" fz="xs" c="dimmed">
-              Completed
-            </Text>
-          </div>
-        }
-      />
-    </Box>
+    <RingProgress
+      roundCaps
+      thickness={6}
+      size={150}
+      sections={[{ value: doneProportion, color: doneProportion < 100 ? 'blue' : 'green' }]}
+      label={
+        <div>
+          <Text ta="center" fz="lg" fw={700} lh={1}>
+            {doneProportion.toFixed(0)}%
+          </Text>
+          <Text ta="center" fz="xs" c="dimmed">
+            Completed
+          </Text>
+        </div>
+      }
+    />
   );
 }
