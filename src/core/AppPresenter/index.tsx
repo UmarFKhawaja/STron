eaa4d8e8d316203129228e0b09886712e6234283
Umar Farooq Khawaja <umar.farooq.khawaja@gmail.com>
2025-08-16 15:38:28 +0100

    refactor: move MantineProvider inside AppPresenter

--- src/core/AppPresenter/index.tsx
+++ src/core/AppPresenter/index.tsx
@@ -1,30 +1,38 @@
+import { localStorageColorSchemeManager, MantineProvider } from '@mantine/core';
 import { Notifications } from '@mantine/notifications';
 import { AddTorrentModal, LogoutModal } from '../../components';
 import { PrivateLayout, PublicLayout } from '../../layouts';
 import { LoginForm, TorrentsView } from '../../outlets';
-import { useCredentials } from '../../providers';
+import { ModalProvider, useCredentials } from '../../providers';
+import { theme } from '../../theme';
+
+const colorSchemeManager = localStorageColorSchemeManager({
+  key: 'color-scheme',
+});
 
 export function AppPresenter() {
   const { hasCredentials } = useCredentials();
 
   return (
-    <>
-      {
-        hasCredentials
-          ? (
-            <PrivateLayout>
-              <TorrentsView/>
-            </PrivateLayout>
-          )
-          : (
-            <PublicLayout>
-              <LoginForm/>
-            </PublicLayout>
-          )
-      }
-      <LogoutModal/>
-      <AddTorrentModal/>
-      <Notifications/>
-    </>
+    <MantineProvider theme={theme} colorSchemeManager={colorSchemeManager} defaultColorScheme="auto">
+      <ModalProvider>
+        {
+          hasCredentials
+            ? (
+              <PrivateLayout>
+                <TorrentsView/>
+              </PrivateLayout>
+            )
+            : (
+              <PublicLayout>
+                <LoginForm/>
+              </PublicLayout>
+            )
+        }
+        <LogoutModal/>
+        <AddTorrentModal/>
+        <Notifications/>
+      </ModalProvider>
+    </MantineProvider>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,21 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { localStorageColorSchemeManager, MantineProvider } from '@mantine/core';
 import { AppPresenter } from './core';
-import { ActionsProvider, CredentialsProvider, ModalProvider, SettingsProvider, TorrentsProvider } from './providers';
-import { theme } from './theme';
+import { ActionsProvider, CredentialsProvider, SettingsProvider, TorrentsProvider } from './providers';
 import '@mantine/core/styles.layer.css';
 import '@mantine/dropzone/styles.layer.css';
 import '@mantine/notifications/styles.layer.css';
 
-const colorSchemeManager = localStorageColorSchemeManager({
-  key: 'color-scheme',
-});
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <MantineProvider theme={theme} colorSchemeManager={colorSchemeManager} defaultColorScheme="auto">
-      <CredentialsProvider>
-        <SettingsProvider>
-          <ActionsProvider>
-            <TorrentsProvider>
-              <ModalProvider>
-                <AppPresenter/>
-              </ModalProvider>
-            </TorrentsProvider>
-          </ActionsProvider>
-        </SettingsProvider>
-      </CredentialsProvider>
-    </MantineProvider>
+    <CredentialsProvider>
+      <SettingsProvider>
+        <ActionsProvider>
+          <TorrentsProvider>
+            <AppPresenter/>
+          </TorrentsProvider>
+        </ActionsProvider>
+      </SettingsProvider>
+    </CredentialsProvider>
   </StrictMode>
 );
